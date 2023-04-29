34c7538f36a1aa38434c24cbe86c3fbbccb42b72
winone44 <winone1208@gmail.com>
2023-04-29 16:31:24 +0200

    increased maximum file size to 100mb

---
+++ public/upload.php
@@ -0,0 +1,29 @@
+<?php
+
+// Sprawdzenie, czy plik został przesłany
+if (!isset($_FILES['file'])) {
+  header('Content-Type: application/json');
+  echo json_encode(['success' => false, 'message' => 'Nie przesłano pliku']);
+  exit();
+}
+
+$file = $_FILES['file'];
+
+// Sprawdzenie, czy plik nie jest zbyt duży
+$maxFileSize = 1024 * 1024 * 100; // 100 MB
+if ($file['size'] > $maxFileSize) {
+  header('Content-Type: application/json');
+  echo json_encode(['success' => false, 'message' => 'Plik jest zbyt duży']);
+  exit();
+}
+
+// Przesunięcie pliku z tymczasowego katalogu do docelowego
+$destination = __DIR__ . '/media/' . $file['name'];
+if (!move_uploaded_file($file['tmp_name'], $destination)) {
+  header('Content-Type: application/json');
+  echo json_encode(['success' => false, 'message' => 'Wystąpił błąd podczas zapisywania pliku']);
+  exit();
+}
+
+header('Content-Type: application/json');
+echo json_encode(['success' => true]);
