77150bbfdd818cc3dea26122fb8590272feb5a65
winone44 <winone1208@gmail.com>
2023-06-09 12:03:37 +0200

    change nav buttons names

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,12 +6,11 @@
         <router-link to="/" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'home'">Home</router-link>
         <router-link to="/register" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'register'"> Dołącz do nas</router-link>
         <router-link to="/login" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'login'">Zaloguj się</router-link>
-
-        <router-link to="/protectedSearchFriend" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-search-friend'">Szukaj znajomych</router-link>
-        <router-link to="/protected-tiktok" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-tiktok'">Dla zalogowanych tiktok</router-link>
-        <router-link to="/protected-mail" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-mail'">Dla zalogowanych wiadomości</router-link>
-        <router-link to="/add-new-video" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'add-new-video'">Dla zalogowanych dodaj nowy film</router-link>
-        <router-link :to="{ name: 'user', params: { id: this.$store.state.userId }}" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'user'">Dla zalogowanych Twój profil</router-link>
+        <router-link to="/protectedSearchFriend" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-search-friend'">Szukaj</router-link>
+        <router-link to="/protected-tiktok" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-tiktok'">Filmy</router-link>
+        <router-link to="/protected-mail" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-mail'">Wiadomości</router-link>
+        <router-link to="/add-new-video" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'add-new-video'">Dodaj nowy film</router-link>
+        <router-link :to="{ name: 'user', params: { id: this.$store.state.userId }}" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'user'">Twój profil</router-link>
         <li @click="logout" class="nav-item" v-if="auth"><b-nav-item>Wyloguj</b-nav-item></li>
     </b-nav>
   </b-sidebar>
