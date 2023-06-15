a8d473eaea442cc80f08bda0527d99bd9f13859d
winone44 <winone1208@gmail.com>
2023-06-15 13:04:31 +0200

    minor code fix

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,13 +3,13 @@
   <b-button variant="info" v-b-toggle.sidebar-right>Menu</b-button>
   <b-sidebar id="sidebar-right" title="Menu" right shadow>
     <b-nav vertical fill>
-        <router-link to="/" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'home'">Home</router-link>
-        <router-link to="/register" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'register'"> Dołącz do nas</router-link>
-        <router-link to="/login" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'login'">Zaloguj się</router-link>
-        <router-link to="/protectedSearchFriend" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-search-friend'">Szukaj</router-link>
-        <router-link to="/protected-tiktok" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-tiktok'">Filmy</router-link>
-        <router-link to="/protected-mail" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-mail'">Wiadomości</router-link>
-        <router-link to="/add-new-video" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'add-new-video'">Dodaj nowy film</router-link>
+        <router-link :to="{ name: 'home'}" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'home'">Home</router-link>
+        <router-link :to="{ name: 'register'}" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'register'"> Dołącz do nas</router-link>
+        <router-link :to="{ name: 'login'}" tag="b-nav-item" class="nav-item" v-if="!auth" :active="$route.name === 'login'">Zaloguj się</router-link>
+        <router-link :to="{ name: 'protected-search-friend'}" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-search-friend'">Szukaj</router-link>
+        <router-link :to="{ name: 'protected-tiktok' }" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-tiktok'">Filmy</router-link>
+        <router-link :to="{ name: 'protected-mail' }" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'protected-mail'">Wiadomości</router-link>
+        <router-link :to="{ name: 'add-new-video' }" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'add-new-video'">Dodaj nowy film</router-link>
         <router-link :to="{ name: 'user', params: { id: this.$store.state.userId }}" tag="b-nav-item" class="nav-item" v-if="auth" :active="$route.name === 'user'">Twój profil</router-link>
         <li @click="logout" class="nav-item" v-if="auth"><b-nav-item>Wyloguj</b-nav-item></li>
     </b-nav>
